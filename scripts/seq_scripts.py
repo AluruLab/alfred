b00af17f720b001805dcfa2ea2eed5c861b40512
Sriram P C <sriram.pc@gmail.com>
2015-07-16 11:46:58 +0530

    updated seq script for bigger sequences

--- scripts/seq_scripts.py
+++ scripts/seq_scripts.py
@@ -13,7 +13,7 @@ cd %s/runs/seq/
 
 
 def gen_4m(seq_4mx2):
-    for k in [0, 1, 2, 3]:
+    for k in [0, 1, 2, 3, 4, 5, 6]:
         script_name = "seq4Mx2.acs.k" + str(k) + ".pbs"
         with open(script_name, "w") as f:
             script_str = script_fmt % (arakawa_path, arakawa_path,
@@ -24,7 +24,21 @@ def gen_4m(seq_4mx2):
             f.write("\n")
 
 
+def gen_23m_50m(seq_big):
+    for k in [0, 1, 2, 3, 4, 5, 6]:
+        script_name = "seqbig.acs.k" + str(k) + ".pbs"
+        with open(script_name, "w") as f:
+            script_str = script_fmt % (arakawa_path, arakawa_path,
+                                       arakawa_path, seq_big[0],
+                                       arakawa_path, seq_big[1],
+                                       'seqbig', 'acs', k, k)
+            f.write(script_str)
+            f.write("\n")
+
+
 if __name__ == '__main__':
     arakawa_path = sys.argv[1]
     seq_4mx2 = ['NC_000962.3.fasta', 'NC_000913.3.fasta']
-    gen_4m(seq_4mx2)
+    seq_big = ['NT_033779.4.fasta', 'NC_019478.1.fasta']
+    # gen_4m(seq_4mx2)
+    gen_23m_50m(seq_big)
